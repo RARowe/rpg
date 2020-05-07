97110ef33d2de108c71ade346d60eb222f82172f
Richard Rowe <richardandrewrowe@gmail.com>
2020-05-07 18:15:31 -0400

    enemy follows you

--- headers/enemy_movement.h
+++ headers/enemy_movement.h
@@ -3,6 +3,8 @@
 #include "enums.h"
 #include "movement_handler.h"
 #include "game_context.h"
+#include "game_math.h"
+
 class EnemyMovement : public MovementHandler
 {
     public:
@@ -15,38 +17,45 @@ class EnemyMovement : public MovementHandler
 		{
             int startX = e.getX();
             int startY = e.getY();
-		    _time += timeStep;
-            bool outsideMap = startX > 19 * 32 || startX < 0 || startY > 13 * 32 || startY < 0;
 
-            if (outsideMap)
+            if (distanceBetween(e, *_context->getPlayer()) < 250.0f)
             {
-                _randomThing = 8;
+                e.move(relativeDirection(e, *_context->getPlayer()), timeStep);
             }
-            if (_time > _randomAmountOfTime)
+            else
             {
-                _randomAmountOfTime = (float)(std::rand() % 4);
-                _randomThing = std::rand() % 8;
-                _time = 0.0f;
-            }
+		        _time += timeStep;
+                bool outsideMap = startX > 19 * 32 || startX < 0 || startY > 13 * 32 || startY < 0;
 
-            switch (_randomThing)
-            {
-                case 0:
-                    e.move(Direction::UP, timeStep);
-                    break;
-                case 1:
-                    e.move(Direction::DOWN, timeStep);
-                    break;
-                case 2:
-                    e.move(Direction::LEFT, timeStep);
-                    break;
-                case 3:
-                    e.move(Direction::RIGHT, timeStep);
-                    break;
-                default:
-                    break;
-            }
+                if (outsideMap)
+                {
+                    _randomThing = 8;
+                }
+                if (_time > _randomAmountOfTime)
+                {
+                    _randomAmountOfTime = (float)(std::rand() % 4);
+                    _randomThing = std::rand() % 8;
+                    _time = 0.0f;
+                }
 
+                switch (_randomThing)
+                {
+                    case 0:
+                        e.move(Direction::UP, timeStep);
+                        break;
+                    case 1:
+                        e.move(Direction::DOWN, timeStep);
+                        break;
+                    case 2:
+                        e.move(Direction::LEFT, timeStep);
+                        break;
+                    case 3:
+                        e.move(Direction::RIGHT, timeStep);
+                        break;
+                    default:
+                        break;
+                }
+            }
             _context->resolveCollision(e, startX, startY);
 		}
 	private:
