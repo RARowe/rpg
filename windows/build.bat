74dc15547b258feea4e202e529394a605ab85b97
rarowe <richardandrewrowe@gmail.com>
2022-11-24 18:47:44 -0600

    Adding sdl2 image dependency.

--- windows/build.bat
+++ windows/build.bat
@@ -1,1 +1,1 @@
-cl /Iinclude main.c /link /libpath:windows\SDL2-dev\lib\x64\ /SUBSYSTEM:WINDOWS shell32.lib SDL2.lib SDL2main.lib
+cl /Iinclude main.c /link /libpath:windows\SDL2-dev\lib\x64\ shell32.lib SDL2.lib SDL2main.lib /libpath:windows\SDL2_image-dev\lib\x64 SDL2_image.lib /SUBSYSTEM:WINDOWS
