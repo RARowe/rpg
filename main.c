cf4fa7cb8da0eff4f4f8386ee9419db2e9feff97
rarowe <richardandrewrowe@gmail.com>
2022-11-25 14:37:21 -0600

    with bitmap library

--- main.c
+++ main.c
@@ -4,8 +4,18 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#include <Windows.h>
+#include <math.h>
 
-int main(int argc, char* argv[]) {
+#include "src/qdbmp.h"
+
+#define ONE_MEG 1024 * 1024
+
+void* allocate() {
+    return VirtualAlloc(NULL, ONE_MEG, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+}
+
+int SDL_INIT() {
     SDL_Window* window = SDL_CreateWindow(
         "Fire",
         SDL_WINDOWPOS_UNDEFINED,
@@ -27,6 +37,18 @@ int main(int argc, char* argv[]) {
         return(EXIT_FAILURE);
     }
 
+    if (TTF_Init()) {
+        puts(SDL_GetError());
+        return(EXIT_FAILURE);
+    }
+
+    if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096)) {
+        puts(Mix_GetError());
+        return(EXIT_FAILURE);
+    }
+
+    //allocate();
+
     SDL_Event event;
     
     while (1) {
@@ -39,5 +61,15 @@ int main(int argc, char* argv[]) {
         }
     }
 
+}
+
+int main(int argc, char* argv[]) {
+    BMP* bmp = BMP_Create(512, 512, 24);
+    for (int i = 0; i < 512; i++) {
+        for (int j = 512; j > 0; j--) {
+            BMP_SetPixelRGB(bmp, i, j, log2(i), j, sin(i) + cos(j));
+        }
+    }
+    BMP_WriteFile(bmp, "test.bmp");
     return(EXIT_SUCCESS);
 }
